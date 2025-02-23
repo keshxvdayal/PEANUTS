63dfbb4b3d2a862c8a0bfa59936227c73ac02977
Mark Mikaelson <79649185+msr8@users.noreply.github.com>
2025-02-23 23:57:47 +0530

    Update settings.py

--- peanut/settings.py
+++ peanut/settings.py
@@ -20,6 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_URL = 'static/'
 
 
 # Quick-start development settings - unsuitable for production
