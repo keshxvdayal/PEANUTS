916f2151ee945aaa9f04230cda89aa08de7e64bc
Mark Mikaelson <79649185+msr8@users.noreply.github.com>
2025-02-23 23:50:12 +0530

    Update settings.py

--- peanut/settings.py
+++ peanut/settings.py
@@ -16,9 +16,11 @@ import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]############
+# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]############
 # STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = BASE_DIR / 'deployment-static-files'
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
