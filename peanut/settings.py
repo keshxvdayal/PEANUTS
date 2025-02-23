2b855c1216161f071ebb8a952266ff0ab4032ee3
Mark Mikaelson <79649185+msr8@users.noreply.github.com>
2025-02-24 00:03:38 +0530

    Update settings.py

--- peanut/settings.py
+++ peanut/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]############
 # STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = BASE_DIR / 'staticfiles-deployment'
 STATIC_URL = 'static/'
 
 
